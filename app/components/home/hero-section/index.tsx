3123f3e1109fd9e2abe6b0617f805e320b6415d7
Thiago Marim <thiago.marim2005@gmail.com>
2024-06-15 20:59:34 -0300

    refactor: adicionar hero section como componente

---
+++ app/components/home/hero-section/index.tsx
@@ -0,0 +1,39 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+import Button from "../../ui/button";
+import { TbBrandGithub, TbBrandLinkedin } from "react-icons/tb";
+
+export default function HeroSection() {
+  return (
+    <section className="flex flex-col items-center text-center gap-4">
+      <h1 className="text-4xl sm:text-5xl font-bold mt-32 sm:mt-[242px]">
+        Thiago Marim
+      </h1>
+      <p className="text-zinc-400 max-w-[527px] text-lg">
+        Desenvolvedor front-end totalmente apaixonado pelo o que faz, atualmente
+        localizado em São Paulo, Brasil.
+      </p>
+      <div className="flex items-center flex-wrap justify-center gap-6">
+        <Button
+          text="Entrar em contato"
+          icon={<HiArrowNarrowRight size={20} />}
+        />
+        <div className="flex items-center gap-4 text-zinc-700">
+          <a
+            href=""
+            target="_blank"
+            className="hover:text-gray-100 transition-colors"
+          >
+            <TbBrandGithub size={24} />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            className="hover:text-gray-100 transition-colors"
+          >
+            <TbBrandLinkedin size={24} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,9 @@
-import { HiArrowNarrowRight } from "react-icons/hi";
-import Button from "./components/ui/button";
-import { TbBrandGithub, TbBrandLinkedin } from "react-icons/tb";
+import HeroSection from "./components/home/hero-section";
 
 export default function Home() {
   return (
     <main className="container flex flex-col gap-32">
-      <section className="flex flex-col items-center text-center gap-4">
-        <h1 className="text-4xl sm:text-5xl font-bold mt-32 sm:mt-[242px]">
-          Thiago Marim
-        </h1>
-        <p className="text-zinc-400 max-w-[527px] text-lg">
-          Desenvolvedor front-end totalmente apaixonado pelo o que faz,
-          atualmente localizado em São Paulo, Brasil.
-        </p>
-        <div className="flex items-center flex-wrap justify-center gap-6">
-          <Button
-            text="Entrar em contato"
-            icon={<HiArrowNarrowRight size={20} />}
-          />
-          <div className="flex items-center gap-4 text-zinc-700">
-            <a
-              href=""
-              target="_blank"
-              className="hover:text-gray-100 transition-colors"
-            >
-              <TbBrandGithub size={24} />
-            </a>
-            <a
-              href=""
-              target="_blank"
-              className="hover:text-gray-100 transition-colors"
-            >
-              <TbBrandLinkedin size={24} />
-            </a>
-          </div>
-        </div>
-      </section>
+      <HeroSection />
     </main>
   );
 }
