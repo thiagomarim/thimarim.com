6c046f0eee0fb8b64b8435dbd17cba44406d7eff
Thiago Marim <thiago.marim2005@gmail.com>
2024-09-26 16:19:42 -0300

    chore: change paragraph content of the about me section

--- app/components/pages/home/about/index.tsx
+++ app/components/pages/home/about/index.tsx
@@ -1,35 +1,44 @@
 import { SectionTitle } from "@/app/components/ui/section-title";
 import Image from "next/image";
 
+const tech_list = [
+  "HTML,",
+  "CSS,",
+  "JavaScript,",
+  "TypeScript,",
+  "Tailwind CSS,",
+  "React,",
+  "Next.js,",
+  "Git,",
+  "Github.",
+];
+
 export default function AboutMe() {
   return (
-    <section className="container flex items-center gap-10 sm:gap-20 flex-wrap lg:flex-nowrap mb-12 sm:mb-40">
+    <section className="container flex flex-wrap items-center gap-10 mb-12 sm:gap-20 lg:flex-nowrap sm:mb-40">
       <div className="flex flex-col gap-4">
         <SectionTitle title="Sobre mim" subtitle="Um pouco mais sobre mim" />
-        <p className="text-paragraph leading-8">
-          Olá eu me chamo Thiago Soares Marim, tenho 19 anos e resido em São
-          Paulo, Brasil. Estou cursando{" "}
-          <span className="text-gray-50">
-            Análise e Desenvolvimento de Sistemas
-          </span>{" "}
-          na{" "}
-          <span className="text-gray-50">
-            Universidade Nove de Julho (UNINOVE)
-          </span>{" "}
-          com a previsão de conclusão para 2026.
+        <p className="leading-8 text-paragraph">
+          Meu nome é Thiago Soares Marim, tenho 19 anos e moro em São Paulo.
+          Atualmente atuo como Desenvolvedor front-end na{" "}
+          <a
+            href="https://shakersagencia.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-50 hover:text-gray-300 transition-colors"
+          >
+            Shakers Agência & Consultoria
+          </a>{" "}
+          e estou cursando Análise e Desenvolvimento de Sistemas na UNINOVE.
         </p>
-        <p className="text-paragraph leading-8">
+        <p className="leading-8 text-paragraph">
           Minha paixão é criar e desenvolver sites com interfaces bonitas e
           atraentes, com foco na experiência do usuário. Possuo conhecimentos em{" "}
-          <span className="text-gray-50">HTML</span>,{" "}
-          <span className="text-gray-50">CSS</span>,{" "}
-          <span className="text-gray-50">JavaScript</span>,{" "}
-          <span className="text-gray-50">TypeScript</span>,
-          <span className="text-gray-50">Tailwind CSS</span>,{" "}
-          <span className="text-gray-50">React</span>,{" "}
-          <span className="text-gray-50">Next.js</span>,{" "}
-          <span className="text-gray-50">Git</span> e{" "}
-          <span className="text-gray-50">Github</span>.
+          {tech_list.map((tech) => (
+            <span key={tech.toLowerCase()} className="text-gray-50">
+              {tech}{" "}
+            </span>
+          ))}
         </p>
       </div>
       <Image
--- app/components/pages/home/featured-projects/index.tsx
+++ app/components/pages/home/featured-projects/index.tsx
@@ -7,7 +7,7 @@ import { SectionTitle } from "@/app/components/ui/section-title";
 export default function FeaturedProjects() {
   return (
     <section className="container my-12 sm:my-40">
-      <div className="flex items-end justify-between mb-10 sm:mb-16 flex-wrap">
+      <div className="flex flex-wrap items-end justify-between mb-10 sm:mb-16">
         <div>
           <SectionTitle
             title="Projetos em Destaque"
@@ -15,7 +15,7 @@ export default function FeaturedProjects() {
           />
         </div>
         <Link href="/all-projects" className="mt-6 sm:mb-0 ">
-          <span className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm">
+          <span className="flex items-center gap-2 text-sm hover:underline hover:underline-offset-4">
             Ver todos <HiArrowNarrowRight size={18} />
           </span>
         </Link>
@@ -25,9 +25,9 @@ export default function FeaturedProjects() {
           <ProjectCard project={project} key={project.name} />
         ))}
       </div>
-      <div className="w-max mt-12 sm:mt-16">
+      <div className="mt-12 w-max sm:mt-16">
         <Link href="/all-projects">
-          <span className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm">
+          <span className="flex items-center gap-2 text-sm hover:underline hover:underline-offset-4">
             Ver todos <HiArrowNarrowRight size={18} />
           </span>
         </Link>
