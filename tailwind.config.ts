1084679a7b011ddec8929cb386ba1c8e96e6e6eb
Thiago Marim <thiago.marim2005@gmail.com>
2024-10-14 18:59:35 -0300

    feat: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "#08090A",
-        title: "#fff",
-        subTitle: "#E3E4E6",
-        paragraph: "#969799",
-        button: "#E6E6E6",
-        buttonText: "#08090A",
-        buttonSecondary: "#232323",
-        ButtonSecondaryText: "#fff",
-        border: "#1C1C1D",
-      },
-      backgroundImage: {
-        "intro-bg": "url('/intro/intro-bg.png')",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
